73864eeb167732148087ba8db5928f33014af542
Guidomp11 <ponceguido@gmail.com>
2020-08-04 10:52:39 -0300

    express agregado

---
+++ src/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const methodOverride =  require('method-override'); 
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(cookieParser());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(methodOverride('_method'));
+app.use(session({secret:'holis'}));
+
+
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+
+
+app.listen(3000, () => console.log("Servidor corriendo en el puerto 3000"));
