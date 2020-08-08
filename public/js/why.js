3f14b3e89919ed9a02b0089864b1e7d8d7014ae2
julia-boot <juliacordero@Julias-MacBook-Pro-2.local>
2020-08-07 22:42:45 -0300

    vista y js de why

--- public/js/why.js
+++ public/js/why.js
@@ -1,1 +1,32 @@
-console.log('ok')
+window.addEventListener('load', function(){
+    let h3 = document.querySelector('.why-h3')
+    let h6 = document.querySelector('.why-h6')
+    let img1 = document.querySelector('.why-img1')
+    let h3_2 = document.querySelector('.why-h3-2')
+    let h6_2 = document.querySelector('.why-h6-2')
+    let img2 = document.querySelector('.why-img2')
+    let h3_3 = document.querySelector('.why-h3-3')
+    let h6_3 = document.querySelector('.why-h6-3')
+    let ultimoDiv = document.querySelector('#text2')
+
+    window.setTimeout(function(){
+        h3.classList.remove('d-none')
+        h6.classList.remove('d-none')
+    }, 1000)
+    window.setTimeout(function(){
+        img1.classList.remove('d-none')
+    }, 2000)
+    window.setTimeout(function(){
+        h3_2.classList.remove('d-none')
+        h6_2.classList.remove('d-none')
+    }, 3000)
+    window.setTimeout(function(){
+        img2.classList.remove('d-none')
+    }, 4000)
+    window.setTimeout(function(){
+        ultimoDiv.classList.remove('d-none')
+        h3_3.classList.remove('d-none')
+        h6_3.classList.remove('d-none')
+    }, 5000)
+
+})
