7e6139b611e43dabedb15027adc79251acd7fef3
julia-boot <juliacordero@Julias-MacBook-Pro-2.local>
2020-08-09 18:30:12 -0300

    js inactividad

--- public/js/header.js
+++ public/js/header.js
@@ -2,6 +2,7 @@ function qs(element) {
     return document.querySelector(element)
 };
 
+
 window.addEventListener('load', function() {
     let btn = document.querySelector('.btnMenu');
     let menuNav = document.querySelector('.sidebar');
