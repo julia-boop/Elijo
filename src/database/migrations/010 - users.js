f3407203e7a11fdda0d702a38c43712c26b6ed5b
Guidomp11 <ponceguido@gmail.com>
2020-10-06 19:27:36 -0300

    merge 1

--- src/database/migrations/010 - users.js
+++ src/database/migrations/010 - users.js
@@ -20,27 +20,12 @@ module.exports = {
       name:{
         type: Sequelize.DataTypes.STRING(100)
       },
-<<<<<<< HEAD
-=======
-      last_name:{
-        type: Sequelize.DataTypes.STRING(100)
-      },
->>>>>>> b22820ab5759ef1c0e044921db8f7b0fc4c52321
       age: {
         type: Sequelize.DataTypes.DATE
       },
       telephone: {
         type: Sequelize.DataTypes.STRING(100)
       },
-<<<<<<< HEAD
-=======
-      adress:{
-        type: Sequelize.DataTypes.STRING(100)
-      },
-      location: {
-        type: Sequelize.DataTypes.STRING(100)
-      },
->>>>>>> b22820ab5759ef1c0e044921db8f7b0fc4c52321
       experiences: {
         type: Sequelize.DataTypes.STRING(255)
       },
