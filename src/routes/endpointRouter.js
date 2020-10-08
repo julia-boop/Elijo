fe3a6b2d57707d2572e1ea4762621a7491ce37d5
Guidomp11 <ponceguido@gmail.com>
2020-10-07 23:35:21 -0300

    dropdown de input en proceso

--- src/routes/endpointRouter.js
+++ src/routes/endpointRouter.js
@@ -8,6 +8,7 @@ const endpointController = require('../controllers/endpointController.js');
 router.get('/careers/:universityOwner', endpointController.getCareers);
 router.get('/courses/:instituteOwner', endpointController.getCourses);
 router.get('/user/:userID', endpointController.getActualUser);
+router.get('/interests', endpointController.getInterestsTable);
 
 
 
