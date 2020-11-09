80bc0b34ed5ab5466958fd5884a4bf105055837b
Guidomp11 <ponceguido@gmail.com>
2020-11-08 22:40:52 -0300

    bsuqeuda por el filtro de universidad y instituto listo

--- public/js/posta/postaNav.js
+++ public/js/posta/postaNav.js
@@ -26,34 +26,50 @@ window.addEventListener('load', () => {
     let universityResults = document.querySelector('.universityResults');
     let instituteResults = document.querySelector('.instituteResults');
 
-    universityInput.addEventListener('change', e => {
-        
-        if(e.target.value == '' || e.target.value == null){
-            console.log('vacio');
+    
+
+    universityInput.addEventListener('change', event => {
+        let amountOfResults = 0;
+        universityResults.innerHTML = ''; 
+        if(event.target.value == '' || event.target.value == null){
             universityResults.innerHTML = '';         
             return;
-        }else{
-            universityResults.innerHTML = 'cargando...';         
         }
         if(universities.length > 0){
-            let inputData = e.target.value.toLowerCase();
-            universityResults.innerHTML = '';
+            let inputData = event.target.value.toLowerCase();
             for(let i = 0; i < universities.length; i++){
                 
                 let univ = universities[i].name.toLowerCase();
                 
                 if(univ.includes(inputData.toLowerCase())){
+                    amountOfResults++;
                     universityResults.innerHTML +=`<option id="inputButton" value="${universities[i].id}">  <a href="#">${universities[i].name}</a> </option>`;
                 }
-                
             }
-            
+            if(amountOfResults <= 0) universityResults.innerHTML = 'No se encontraron resultados';
         }
     });
-    instituteInput.addEventListener('change', e => {
-        if(e.target.value == '' || e.target.value == null){
+
+    instituteInput.addEventListener('change', event => {
+        let amountOfResults = 0;
+        instituteResults.innerHTML = '';
+        if(event.target.value == '' || event.target.value == null){
             instituteResults.innerHTML = '';         
             return;
+        }
+        if(institutes.length > 0){
+            let inputData = event.target.value.toLowerCase();
+            for(let i = 0; i < institutes.length; i++){
+                
+                let inst = institutes[i].name.toLowerCase();
+                
+                if(inst.includes(inputData.toLowerCase())){
+                    amountOfResults++;
+                    instituteResults.innerHTML +=`<option id="inputButton" value="${institutes[i].id}">  <a href="#">${institutes[i].name}</a> </option>`;
+                }
+            }
+
+            if(amountOfResults <= 0) instituteResults.innerHTML = 'No se encontraron resultados';
         }
     });
 });
