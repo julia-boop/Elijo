4a34b565e878988c15d53b8e07822d0ae7bd14dd
Guidomp11 <ponceguido@gmail.com>
2020-11-17 12:01:11 -0300

    trabajando en intereses

--- src/ownModules/filterUsers.js
+++ src/ownModules/filterUsers.js
@@ -174,7 +174,7 @@ function filterByInterest(users, query){
     for(let i=0; i<users.length; i++){
         for(let j=0; j< query.length; j++){
             for(let k = 0; k < users[i].Interest.length; k++){
-                if(users[i].Interest[k].toLowerCase() == query[j].toLowerCase()){
+                if(users[i].Interest[k].dataValues.name.toLowerCase() == query[j].toLowerCase()){
                     usersByInterests.push(users[i]);
                 }
             }
