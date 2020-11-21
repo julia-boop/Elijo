8accdbbe1efe90aaf91665e38aec8c28fe90e990
julia-boot <juliacordero@MacBook-Pro.local>
2020-11-21 17:04:04 -0300

    validaciones de los formularios de usuario listos

---
+++ public/js/answer.js
@@ -0,0 +1,34 @@
+function qs(element) {
+    return document.querySelectorAll(element)
+};
+
+window.addEventListener('load', function(){
+    let ansForm = qs('.ansForm');
+    let ansSelect = qs('.ansSelect');
+    let ansText = qs('.ansText');
+    let ansBtn = qs('.ansBtn');
+    let erSel = qs('.erSel');
+    let erAns = qs('.erAns');
+    
+    ansBtn.addEventListener('click', function(e){
+        e.preventDefault();
+
+        let errors = {};
+
+        if(ansSelect.value == ''){
+            errors.select = 'Tenes que seleccionar una pregunta'
+        }
+
+        if(ansText.value.length < 1){
+            errors.text = 'Tenes que escribir una respuesta'
+        }
+
+        if(Object.keys(errors).length >= 1){
+            erSel.innerText = (errors.select != '') ? errors.select : '     '
+            erAns.innerText = (errors.text != '') ? errors.text : '    '
+        } else {
+            calForm.submit();
+        }        
+
+    })
+})
